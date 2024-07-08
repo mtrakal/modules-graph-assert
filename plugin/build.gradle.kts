04e71be9206d7988250e06920eb573d3107a7f4f
Matej Trakal <mtrakal@gmail.com>
2024-07-08 23:21:16 +0200

    Update readme / build gradle

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -12,20 +12,20 @@ dependencies {
   implementation(libs.junit)
 }
 
-group = "com.jraska.module.graph.assertion"
+group = "cz.mtrakal.module.graph.assertion"
 
 gradlePlugin {
-  website = "https://github.com/jraska/modules-graph-assert"
-  vcsUrl = "https://github.com/jraska/modules-graph-assert"
+  website = "https://github.com/mtrakal/modules-graph-assert"
+  vcsUrl = "https://github.com/mtrakal/modules-graph-assert"
 
   plugins {
     create("modulesGraphAssert") {
-      id = "com.jraska.module.graph.assertion"
+      id = "cz.mtrakal.module.graph.assertion"
       version = "3.0.0"
       displayName = "Modules Graph Assert"
       description = "Gradle plugin to keep your modules graph healthy and lean."
       implementationClass = "com.jraska.module.graph.assertion.ModuleGraphAssertionsPlugin"
-      tags.addAll("graph", "assert", "build speed", "android(", ")java", "kotlin", "quality", "multiprojects", "module")
+      tags.addAll("graph", "assert", "build speed", "android", "java", "kotlin", "quality", "multiprojects", "module")
     }
   }
 }
